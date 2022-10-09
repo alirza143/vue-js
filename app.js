d713e24f510da4f8d9a8b03a581ebd0405453ae4
Your Name <you@example.com>
2022-10-09 18:09:42 +0500

    handle different mouse event

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ const app = Vue.createApp({
             title : "this is new test",
             author : "Ali Raza",
             age : "25",
+            x:0,
+            y:0
         }
     },
     methods : {
         toggleShowBooks(){
             this.showBooks = !this.showBooks;
+        },
+
+        handleEvent(e,data){
+            console.log(e.type);
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 });
