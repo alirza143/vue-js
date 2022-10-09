ce2d7921693ad8d665ca5474ee7e67919d830f09
Your Name <you@example.com>
2022-10-09 19:57:29 +0500

    list a array or object or data

--- app.js
+++ app.js
@@ -3,28 +3,17 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks : true,
-            title : "this is new test",
-            author : "Ali Raza",
-            age : "25",
-            x:0,
-            y:0
+            books : [
+                { title: 'name of the wind', author: 'patrick rothfuss' },
+                { title: 'the way of kings' , author: ' brandon sanderson' },
+                {title: 'the final empire' , author: ' brandon sanderson' },
+            ]
         }
     },
     methods : {
         toggleShowBooks(){
             this.showBooks = !this.showBooks;
         },
-
-        handleEvent(e,data){
-            console.log(e.type);
-            if(data){
-                console.log(data)
-            }
-        },
-        handleMouseMove(e){
-            this.x = e.offsetX;
-            this.y = e.offsetY;
-        }
     }
 });
 
